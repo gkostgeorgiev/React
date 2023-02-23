bee9545ccfb341bd10c71bbe85b17c024853f284
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2023-02-23 17:52:25 +0200

    Implemented dummy user base and search
    Implemented asynchronous request to fetch dummy data from jsonplaceholder.
    Implemented basic search functionality that needs to be improved.

--- ghoul-catalog/src/App.js
+++ ghoul-catalog/src/App.js
@@ -1,25 +1,51 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      ghouls: [],
+    };
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then((users) => this.setState(() => {
+        return { ghouls: users };
+      },
+        console.log(this.state)))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    console.log('render');
+    return (
+      <div className="App">
+        <input className='search-box' type='search' placeholder='Search ghouls' onChange={(event) => {
+          const searchString = event.target.value.toLowerCase();
+          const result = this.state.ghouls.filter((ghoul) => {
+            return ghoul.name.toLowerCase().includes(searchString);
+          });
+
+          this.setState(() => {
+            return { ghouls: result };
+          });
+        }} />
+        {
+          this.state.ghouls.map((ghoul) => {
+            return <div key={ghoul.id}><h1>{ghoul.name}</h1></div>;
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;

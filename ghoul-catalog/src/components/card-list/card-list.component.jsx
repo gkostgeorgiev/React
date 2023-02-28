9bfd5187de93e196f30e4cba23b1956c0802661a
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2023-02-28 19:08:02 +0200

    Created CSS style files for each component
    Improved the styling of the components. Optimized the card list component.

--- ghoul-catalog/src/components/card-list/card-list.component.jsx
+++ ghoul-catalog/src/components/card-list/card-list.component.jsx
@@ -1,17 +1,25 @@
 import { Component } from "react";
+import './card-list.styles.css';
 
 class CardList extends Component {
 
     render() {
-        console.log(this.props.ghouls);
-        console.log('render from Cardlist');
         const { ghouls } = this.props;
 
         return (
-            <div className="Card-list">
-                {ghouls.map(ghoul => (
-                    <h1 key={ghoul.id} >{ghoul.name}</h1>
-                ))}
+            <div className="card-list">
+                {ghouls.map((ghoul) => {
+                    const { name, email, id } = ghoul;
+                    return (
+                        <div className="card-container" key={id}>
+                            <img
+                                alt={`ghoul ${name}`}
+                                src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    );
+                })}
             </div>
         );
     }

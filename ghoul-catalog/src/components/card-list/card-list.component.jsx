052714fe0e0962ae2722ae2ad90521d360965a34
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2023-03-02 18:44:40 +0200

    Card-list component converted
    Converted card-list component from a class component to a functional component

--- ghoul-catalog/src/components/card-list/card-list.component.jsx
+++ ghoul-catalog/src/components/card-list/card-list.component.jsx
@@ -2,21 +2,30 @@ import { Component } from "react";
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-class CardList extends Component {
+const CardList = ({ ghouls }) => (
+    <div className="card-list">
+        {ghouls.map(ghoul => {
+            return <Card ghoul={ghoul} />;
+        })}
+    </div>
+);
 
-    render() {
-        const { ghouls } = this.props;
 
-        return (
-            <div className="card-list">
-                {ghouls.map((ghoul) => {
-                    return (
-                        <Card ghoul={ghoul} />
-                    );
-                })}
-            </div>
-        );
-    }
-}
+// class CardList extends Component {
+
+//     render() {
+//         const { ghouls } = this.props;
+
+//         return (
+//             <div className="card-list">
+//                 {ghouls.map((ghoul) => {
+//                     return (
+//                         <Card ghoul={ghoul} />
+//                     );
+//                 })}
+//             </div>
+//         );
+//     }
+// }
 
 export default CardList;

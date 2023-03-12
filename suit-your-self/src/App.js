f14bbecd79d9e3150de4671779c8d33aeca847d4
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2023-03-12 15:38:21 +0200

    Installed React Router
    Moved the landing page into a separate home component.

--- suit-your-self/src/App.js
+++ suit-your-self/src/App.js
@@ -1,38 +1,7 @@
-import './categories.styles.scss';
-import Directory from './components/directory/directory.component';
+import Home from './routes/home/home.component';
 
 const App = () => {
-  const categories = [
-    {
-        id: 1,
-        title: 'Headwear',
-        imageUrl: 'https://i.ibb.co/n0G5ct2/pexels-pixabay-35185.jpg',
-    },
-    {
-        id: 2,
-        title: 'Coats',
-        imageUrl: 'https://i.ibb.co/jhp22Gj/pexels-andrea-piacquadio-840916.jpg',
-    },
-    {
-        id: 3,
-        title: 'Shoes',
-        imageUrl: 'https://i.ibb.co/FJ7fS7T/pexels-dominika-roseclay-1021816.jpg',
-    },
-    {
-        id: 4,
-        title: 'Ladies',
-        imageUrl: 'https://i.ibb.co/dpLV2zB/pexels-radu-florin-2646841.jpg',
-    },
-    {
-        id: 5,
-        title: 'Gentlemen',
-        imageUrl: 'https://i.ibb.co/3SBX8Gj/pexels-jordan-bergendahl-10402661.jpg',
-    },
-];
-
-  return (
-    <Directory categories={categories} />
-  );
+  return <Home />;
 };
 
 export default App;
